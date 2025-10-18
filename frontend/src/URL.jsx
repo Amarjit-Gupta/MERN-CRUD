68bc5bd27f99fdbff1802bdb33df93dc4c593055
Amarjit-Gupta <akg433787@gmail.com>
2025-10-18 23:39:24 +0530

    second-commit

--- frontend/src/URL.jsx
+++ frontend/src/URL.jsx
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:5000";
+// export const URL = "http://localhost:5000";
 
-// export const URL = "https://mern-auth-crud-ashen.vercel.app";
+export const URL = "https://mern-crud-ashen-chi.vercel.app";
